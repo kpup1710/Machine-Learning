fe28ca210e5137b27480492f3e8950bbe6615863
Dung <nguyenvietd67@gmail.com>
2023-01-17 16:35:45 +0700

    mess

--- Custom_dataloader/utils.py
+++ Custom_dataloader/utils.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 from tqdm import tqdm
 import pandas as pd
-from configs import*
+from Custom_dataloader.configs import*
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
